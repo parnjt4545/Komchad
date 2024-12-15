10367d0d072f3d93b05dec764e0a0b9afb61a568
parnjt4545 <Komchadmankarn2002@gmail.com>
2024-12-16 06:57:29 +0700

    my first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/* ======================== typing animation ===================== */
+var typed = new Typed(".typing",{
+    strings:["","Graphic Design","Web Design"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true       
+})
